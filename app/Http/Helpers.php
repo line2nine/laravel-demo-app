24700bf1baf9608b7bf6ac913fc38e309688b962
snakeback <61499665+line2nine@users.noreply.github.com>
2022-09-09 14:09:25 +0700

    Update Helpers.php

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -11,3 +11,40 @@ function noNeg(array $arr)
         }
         return $newArr;
     }
+
+function maxMinAvg(array $arr)
+    {
+        $newArr = [];
+        $sum = 0;
+        foreach ($arr as $val) {
+            $sum += $val;
+        }
+        $newArr[] = max($arr);
+        $newArr[] = min($arr);
+        $newArr[] = $sum / count($arr);
+    
+        return $newArr;
+    }
+
+function swap(array $arr)
+    {
+        $newArr = array_slice($arr, 0);
+        $first = $newArr[0];
+        $last = $newArr[count($newArr) - 1];
+        $newArr[0] = $last;
+        $newArr[count($newArr) - 1] = $first;
+        
+        return $newArr;
+    }
+
+function numToStr(array $arr)
+    {
+        $newArr = [];
+        foreach ($arr as $val) {
+            if ($val < 0) {
+                $val = "Dojo";
+            }
+            $newArr[] = $val;
+        }
+        return $newArr;
+    }
